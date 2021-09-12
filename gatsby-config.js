65ca04102745e38da7e71e37e6bc8130ab7b795a
bekishore <bekishore@gmail.com>
2021-09-12 09:33:40 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,11 +28,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/bekishore`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `http://www.kishore.org`,
           },
         ],
       },
